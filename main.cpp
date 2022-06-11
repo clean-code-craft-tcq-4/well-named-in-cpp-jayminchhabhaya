6b1af2292d726f2b2c732bacb580849743cc057a
jayminchhabhaya <jayamin.chhabhaya11@gmail.com>
2022-06-11 18:16:37 +0530

    Update code for Print Pair Number and Color Pairs

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ int main() {
 	for(int i = 1 ;i <=(numberOfMajorColors * numberOfMinorColors);++i)
 	{
 		ColorPair colorPair = GetColorFromPairNumber(i);
-		std::cout <<i<<"   "colorPair.ToString()<<std::endl;
+		std::cout <<i<<"   "<<colorPair.ToString()<<std::endl;
 	}
     return 0;
 }
