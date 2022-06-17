9914699340543115f5e598b307a3648d6dbea75b
jayminchhabhaya <jayamin.chhabhaya11@gmail.com>
2022-06-17 11:02:53 +0530

    add changes as per review

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include"TelecommunicationsColor.hpp"
 using namespace TelecommunicationsColor;
 
+TelecommunicationsColor::ColorPair *ObjectOfColorPair{};
 void testNumberToPair(int pairNumber,
    MajorColor expectedMajor,
    MinorColor expectedMinor)
 {
-	TelecommunicationsColor::ColorPair ObjectOfColorPair;
-    TelecommunicationsColor::ColorPair colorPair = ObjectOfColorPair.GetColorFromPairNumber(pairNumber);
+    TelecommunicationsColor::ColorPair colorPair = ObjectOfColorPair->GetColorFromPairNumber(pairNumber);
     std::cout << "Got pair " << colorPair.ToString() << std::endl;
     assert(colorPair.getMajor() == expectedMajor);
     assert(colorPair.getMinor() == expectedMinor);
@@ -19,8 +19,7 @@ void testPairToNumber(
     MinorColor minor,
     int expectedPairNumber)
 {
-	TelecommunicationsColor::ColorPair ObjectOfColorPair;
-    int pairNumber = ObjectOfColorPair.GetPairNumberFromColor(major, minor);
+    int pairNumber = ObjectOfColorPair->GetPairNumberFromColor(major, minor);
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
 }
@@ -29,8 +28,7 @@ int main() {
     testNumberToPair(5, WHITE, SLATE);
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-    TelecommunicationsColor::ColorPair ObjectOfColorPair{};
-	ObjectOfColorPair.print_colorpair();
+	ObjectOfColorPair->print_colorpair();
     return 0;
 }
 
