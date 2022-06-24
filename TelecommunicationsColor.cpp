2020af77285b60a35374569dc85eac47c9feb9c3
APAC\CJA2KOR <cja2kor@bosch.com>
2022-06-24 10:52:36 +0530

    changes as per rquirement

--- TelecommunicationsColor.cpp
+++ TelecommunicationsColor.cpp
@@ -1,25 +1,15 @@
 #include <string>
 #include <iostream>
 #include "TelecommunicationsColor.hpp"
-namespace TelecommunicationsColor
-{
-    const char* MajorColorNames[] = {
-        "White", "Red", "Black", "Yellow", "Violet"
-    };
-    int numberOfMajorColors =
-        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-    const char* MinorColorNames[] = {
-        "Blue", "Orange", "Green", "Brown", "Slate"
-    };
-    int numberOfMinorColors =
-        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
+namespace TelecommunicationsColor{
+    const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
+    int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+    int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 	ColorPair ColorPair::GetColorFromPairNumber(int pairNumber) {
     int BasedPairNumber = pairNumber - 1;
-    MajorColor majorColor = 
-        (MajorColor)(BasedPairNumber / numberOfMinorColors);
-    MinorColor minorColor =
-        (MinorColor)(BasedPairNumber % numberOfMinorColors);
+    MajorColor majorColor = (MajorColor)(BasedPairNumber / numberOfMinorColors);
+    MinorColor minorColor = (MinorColor)(BasedPairNumber % numberOfMinorColors);
     return ColorPair(majorColor, minorColor);
     }
     int ColorPair::GetPairNumberFromColor(MajorColor major, MinorColor minor) {
@@ -31,36 +21,29 @@ namespace TelecommunicationsColor
         colorPairStr += MinorColorNames[minorColor];
         return colorPairStr;
     }
-	void ColorPair::prepare_string(int PaitNo , std::string str_colorpair)
-	{
+	void ColorPair::prepare_string(int PaitNo , std::string str_colorpair){
 		std::string prt_clrpair = "|   "+ std::to_string(PaitNo) +" \t   |  "+ str_colorpair + "\t |"
 		print_colorpair(prt_clrpair);
-		if(PaitNo%5 == 0)
-		{
+		if(PaitNo%5 == 0){
 			print_colorpair("----------------------------------------");
 		}
 	}
-	void ColorPair::GetClrPair()
-	{
-		for(int i = 1 ;i <=(numberOfMajorColors * numberOfMinorColors);++i)
-		{
+	void ColorPair::GetClrPair(){
+		for(int i = 1 ;i <=(numberOfMajorColors * numberOfMinorColors);++i){
 			ColorPair colorPair = GetColorFromPairNumber(i);
 			prepare_string(i,colorPair.ToString());
-			if(i%5 == 0)
-			{
+			if(i%5 == 0){
 				std::cout << "----------------------------------------" << std::endl;
 			}
 		}
 	}
-	void ColorPair::Print_header()
-	{
+	void ColorPair::Print_header(){
 		std::cout << "25-pair color code" << std::endl;
 		std::cout << "----------------------------------------" << std::endl;
 		std::cout << "| Pair no. | Major color | Minor color |" << std::endl;
 		std::cout << "----------------------------------------" << std::endl;
 	}
-	void ColorPair::print_colorpair(std::string str_colorpair)
-	{
+	void ColorPair::print_colorpair(std::string str_colorpair){
 		std::cout << str_colorpair << std::endl;	
 	}
 }
